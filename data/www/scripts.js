3e435a4171a98b9a0960ae58b14bc98d76b14b8a
Tilman Justen <git@tilmanjusten.de>
2018-10-18 18:39:15 +0200

    update UI based on state

--- data/www/scripts.js
+++ data/www/scripts.js
@@ -1,33 +1,48 @@
 const devices = [{
   'id': 0,
   'name': 'Alle Lichter',
-  'name_id': 'alle-lichter'
+  'name_id': 'alle-lichter',
+  'state': 'off'
 }, {
   'id': 1,
   'name': 'Wohnzimmerlicht',
-  'name_id': 'wohnzimmerlicht'
+  'name_id': 'wohnzimmerlicht',
+  'state': 'off'
 }, {
   'id': 2,
   'name': 'Sofa & Regal',
-  'name_id': 'sofalicht'
+  'name_id': 'sofalicht',
+  'state': 'off'
 }, {
   'id': 3,
   'name': 'Lichterkette',
-  'name_id': 'lichterkette'
+  'name_id': 'lichterkette',
+  'state': 'off'
 }, {
   'id': 4,
   'name': 'Türlicht',
-  'name_id': 'tuerlicht'
+  'name_id': 'tuerlicht',
+  'state': 'off'
 }, {
   'id': 5,
   'name': 'Galerielicht',
-  'name_id': 'galerielicht'
+  'name_id': 'galerielicht',
+  'state': 'off'
 }];
+
+const updateUI = () => {
+  devices.forEach(device => {
+    const button = document.querySelector(`.switch--${device.name_id}`)
+    const removeClassModifier = device.state === 'on' ? 'off' : 'on'
+
+    button.classList.remove(`switch--${removeClassModifier}`)
+    button.classList.add(`switch--${device.state}`)
+  })
+}
 
 // Get current state
 (function () {
   devices.forEach(device => {
-    const button = document.querySelector(`.switch--${device.name_id}`)
     const xhr = new XMLHttpRequest()
 
     xhr.open('GET', `/state/${device.name_id}`)
@@ -35,10 +50,10 @@ const devices = [{
     xhr.onload = function () {
       if (xhr.status === 200) {
         const response = JSON.parse(xhr.response)
-        const removeClassModifier = response.state === 'on' ? 'off' : 'on';
 
-        button.classList.remove(`switch--${removeClassModifier}`)
-        button.classList.add(`switch--${response.state}`)
+        device.state = response.state
+
+        updateUI()
       } else {
         console.error('Something went wrong getting the state of %s', device.name)
         console.table(device)
@@ -51,28 +66,43 @@ const devices = [{
 })()
 
 // Toggle switches on user interaction
+const switchOffByDeviceNameId = name_id => {
+  const deviceIndex = devices.findIndex(device => device.name_id === name_id)
+
+  devices[deviceIndex].state = 'off'
+}
+
 const toggleSwitch = evt => {
   const button = evt.target
   const state = button.classList.contains('switch--on')
   const targetState = state ? 'off' : 'on'
   const deviceName = button.dataset.name
   const xhr = new XMLHttpRequest()
+  const deviceIndex = devices.findIndex(device => device.name_id === deviceName)
 
   xhr.open('GET', `/switch/${deviceName}/${targetState}`)
   xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
   xhr.onload = function () {
     if (xhr.status === 200) {
       const response = JSON.parse(xhr.response)
-      const removeClassModifier = response.state === 'on' ? 'off' : 'on';
 
-      button.classList.remove(`switch--${removeClassModifier}`)
-      button.classList.add(`switch--${response.state}`)
+      devices[deviceIndex].state = response.state
+
+      // update device groups
+      if (deviceName === 'alle-lichter') {
+        ['wohnzimmerlicht', 'lichterkette', 'galerielicht', 'tuerlicht', 'sofalicht'].forEach(switchOffByDeviceNameId)
+      } else if (deviceName === 'wohnzimmerlicht') {
+        ['lichterkette', 'sofalicht'].forEach(switchOffByDeviceNameId)
+      }
+
+      updateUI()
     } else {
       console.error('Something went wrong when setting the state of %s', device.name)
       console.table(button)
       console.dir(xhr)
     }
   }
+
   xhr.send()
 }
 
